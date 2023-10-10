d599e2ac775a6f562ce742d026329895900c83f5
MuskanMakandar <146813804+MuskanMakandar@users.noreply.github.com>
2023-10-10 12:38:29 +0530

    QuestionAnswer.java

---
+++ QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "A computer communicate with other computers on the internet through?",
+            "Which of the following is not a valid domain name?",
+            "Who is the father of C language?",
+            "Which company owns the android?"
+    };
+
+    public static String choice[][]={
+            {"IP","TCP/IP","HTTPS","Web browser"},
+            {"www.yahoo.com","www.jahoo.co.uk","www.com.yahoo","www.yahoo.co.in"},
+            {"Steve jobs","James Gosling","Dennish Ritchie","Rasmus Lerdorf"},
+            {"Google","Apple","nokia","Samsung"}
+
+    };
+
+    public static String correctAnswers[]={
+            "TCP/IP",
+            "www.com.yahoo",
+            "Dennish Ritchie",
+            "Google"
+    };
+
+}
